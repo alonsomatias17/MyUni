dcfed47c63896b534387391c4f2db24a7e1407b4
alonsomatias17 <alonso.matias17@gmail.com>
2018-10-20 16:24:20 -0300

    menu

--- MyUniApplication/app/src/main/java/com/alonso/myuniapplication/MenuActivity.java
+++ MyUniApplication/app/src/main/java/com/alonso/myuniapplication/MenuActivity.java
@@ -1,12 +1,16 @@
 package com.alonso.myuniapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout menuDrawerLayout;
     private ActionBarDrawerToggle menuActionBarDrawerToggle;
@@ -23,6 +27,10 @@ public class MenuActivity extends AppCompatActivity {
         menuActionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flMain, new MainMenuFragment());
+        fragmentTransaction.commit();*/
     }
 
     @Override
@@ -31,5 +39,28 @@ public class MenuActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_account:
+                this.startActivity(new Intent(this, ProfileActivity.class));
+                break;
+            case R.id.nav_asignaturas_aprobadas:
+                // another startActivity, this is for item with id "menu_item2"
+                break;
+            case R.id.nav_asignaturas_curso:
+                // another startActivity, this is for item with id "menu_item2"
+                break;
+            case R.id.nav_info:
+                // another startActivity, this is for item with id "menu_item2"
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+        return true;
     }
 }
